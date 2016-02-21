41262e4c12c3d1caff31a1999a404fc86fd2fd56
Rob Audis <rob@raudis.co.uk>
2016-02-21 21:24:20 +0000

    Adding splitting of files with datetime as name. Also adding annotation text to the video (only time for now)

--- dashcam.py
+++ dashcam.py
@@ -1,7 +1,19 @@
 import picamera
+import datetime as dt
 
 with picamera.PiCamera() as camera:
     camera.resolution = (1280, 720)
-    camera.start_recording('/dashcam-videos/my_video.h264')
-    camera.wait_recording(30)
+    camera.framerate = 24
+    camera.annotate_background = picamera.Color('black')
+    camera.annotate_text = dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+    camera.start_recording("/dashcam-videos/%s.h264" % dt.datetime.now().strftime("%Y%m%dT%H%M%S"))
+    start = dt.datetime.now()
+    for i in range(0,10):
+        while (dt.datetime.now() - start).seconds < 5:
+            camera.annotate_text = dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            camera.wait_recording(0.2)
+        camera.split_recording("/dashcam-videos/%s.h264" % dt.datetime.now().strftime("%Y%m%dT%H%M%S"))
+        start = dt.datetime.now()
+        
     camera.stop_recording()
