d9669463305498254c5d7a77cb4acbf0c5cb417a
Rob Audis <rob@raudis.co.uk>
2016-02-21 15:49:05 +0000

    initial commit

---
+++ dashcam.py
@@ -0,0 +1,7 @@
+import picamera
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (1280, 720)
+    camera.start_recording('/dashcam-videos/my_video.h264')
+    camera.wait_recording(60)
+    camera.stop_recording()
