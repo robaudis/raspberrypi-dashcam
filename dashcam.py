245f5159382450c710251ca850af9e6f4d3ecc3e
Rob Audis <rob@raudis.co.uk>
2016-02-21 17:27:10 +0000

    testing ssh

--- dashcam.py
+++ dashcam.py
@@ -3,5 +3,5 @@ import picamera
 with picamera.PiCamera() as camera:
     camera.resolution = (1280, 720)
     camera.start_recording('/dashcam-videos/my_video.h264')
-    camera.wait_recording(60)
+    camera.wait_recording(30)
     camera.stop_recording()
